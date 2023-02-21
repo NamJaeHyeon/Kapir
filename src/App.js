afc857f58534e69013fae5ed509387d88f454a0c
jaehyeon <rexnamccx1229@gmail.com>
2023-02-21 14:27:50 +0900

    version 0

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="in-app">width : 1140px</div>
+    </div>
+  );
+}
+
+export default App;
